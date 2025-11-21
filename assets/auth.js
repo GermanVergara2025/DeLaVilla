9ee81107646a642cfe802e1944497b35a28fe2a6
GermanVergara2025 <est.german.vergara@unimilitar.edu.co>
2025-11-20 20:28:29 -0500

    Update auth.js

--- assets/auth.js
+++ assets/auth.js
@@ -1,10 +1,11 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-analytics.js";
+import { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-analytics.js";
 import { 
   getAuth, 
   onAuthStateChanged, 
   signOut 
 } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBKWj1As-nnkcX8aZ7B0AbtzZoQ-56qTjc",
@@ -16,8 +17,12 @@ const firebaseConfig = {
     measurementId: "G-XB6BTBJGND"
 };
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db   = getFirestore(app);
+
+export { app, auth, db };
+
 
 export function logout() {
   signOut(auth).then(() => {
